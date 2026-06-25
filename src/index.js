82657488f4c3cc5e82f76c7b407b7c32058162e1
aditya8948 <89adityapandey@gmail.com>
2026-06-25 17:20:29 +0530

    Added basic user model

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded());
+
+ 
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
